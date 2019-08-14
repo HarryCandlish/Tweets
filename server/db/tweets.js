f063b35dd7cda1a29e821576187db1cca1d9d7b2
HarryCandlish <hcnd90@gmail.com>
2019-08-14 13:29:17 +1200

    displaying tweet data

--- server/db/tweets.js
+++ server/db/tweets.js
@@ -1,1 +1,32 @@
 const connection = require("./connection");
+
+// data base queries
+
+function getTweets(testDb) {
+  const db = testDb || connection;
+  return db("tweets").select();
+}
+
+function createTweet(tweet, testDb) {
+  const db = testDb || connection;
+
+  return db("tweet").insert(tweet);
+}
+
+// Two Step Query
+
+function getTweetsByUsername(username, testDb) {
+  const db = testDb || connection;
+
+  return db("users")
+    .where("username", username)
+    .then(user => {
+      return db("tweets").where("user_id", user.id);
+    });
+}
+
+module.exports = {
+  getTweets,
+  createTweet,
+  getTweetsByUsername
+};
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const { getTweets } = require("../db/tweets");
+
 // GET /api/v1/tweets
 router.get("/", (req, res) => {
-  res.json([
-    {
-      id: 5,
-      text: "this is a tweet"
-    }
-  ]);
+  getTweets()
+    .then(tweets => {
+      res.json(tweets);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Something went wrong" });
+    });
 });
 
-// POST /api/v1/tweets
+// POST / api / v1 / tweets;
 router.get("/", (req, res) => {
   res.json({
     id: 6
